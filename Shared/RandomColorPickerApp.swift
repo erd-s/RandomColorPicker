d399dbd0a6f6f81a4180c9419465f6af4e020294
Chris Erdos <chris.erdos@gmail.com>
2021-09-13 20:05:33 -0400

    Initial Commit

---
+++ Shared/RandomColorPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomColorPickerApp.swift
+//  Shared
+//
+//  Created by Christopher Erdos on 9/13/21.
+//
+
+import SwiftUI
+
+@main
+struct RandomColorPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
